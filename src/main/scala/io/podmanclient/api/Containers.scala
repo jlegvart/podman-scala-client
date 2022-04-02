d359c06689b659f002bf72b27995ac4a16a49975
jlegvart <jernej.legvart@protonmail.com>
2022-04-02 11:16:42 +0200

    containers API

--- src/main/scala/io/podmanclient/api/Containers.scala
+++ src/main/scala/io/podmanclient/api/Containers.scala
@@ -1,25 +1,25 @@
 package io.podmanclient.api
 
 import cats._
+import cats.effect.Concurrent
 import cats.effect._
 import cats.syntax.all._
-import cats.effect.Concurrent
-import org.http4s.client.Client
-import io.podmanclient.api.response.PodmanResponse
-import io.podmanclient.client.PodmanClient._
-import io.podmanclient.api.uri.PodmanUri._
-import org.http4s.circe._
-import org.http4s.client.dsl.io._
-import org.http4s.client._
-import org.http4s.implicits._
-import io.circe.syntax._
 import io.circe.Json
 import io.circe.generic.auto._
+import io.circe.syntax._
 import io.podmanclient._
-import org.http4s.Request
-import org.http4s.Method
+import io.podmanclient.api.response.PodmanResponse
 import io.podmanclient.api.response.ResponseSuccess
+import io.podmanclient.api.uri.PodmanUri._
+import io.podmanclient.client.PodmanClient._
+import org.http4s.Method
+import org.http4s.Request
 import org.http4s.Uri
+import org.http4s.circe._
+import org.http4s.client.Client
+import org.http4s.client._
+import org.http4s.client.dsl.io._
+import org.http4s.implicits._
 
 object Containers {
 
@@ -62,14 +62,16 @@ object Containers {
 
   def start[F[_]: Concurrent](
     prefix: String,
-    name: String)(
+    name: String,
+  )(
     client: Client[F]
   ): F[PodmanResponse[Json]] = {
-    val u = Uri.unsafeFromString(prefix) / "containers" / name / "start"
+    val u                   = Uri.unsafeFromString(prefix) / "containers" / name / "start"
     val request: Request[F] = Request[F](Method.POST, u)
-      
+
     client.expect[Json](request).map(json => ResponseSuccess(Some(json)))
-  } 
+  }
+
 }
 
 case class PortMapping(
